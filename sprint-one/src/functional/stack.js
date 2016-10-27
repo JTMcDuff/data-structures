6ca0a0e92f6ef774db4e3411cd93876a8236d624
MKS-students <mks.students@hackreactor.com>
2016-10-26 19:25:28 -0500

    Completed Functional Stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,31 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var sizeOf = someInstance.size();
+    if (sizeOf === 0) {
+      storage[0] = value;
+    } else {
+      storage[sizeOf] = value;
+    }
   };
 
   someInstance.pop = function() {
+    var sizeOf = someInstance.size();
+    var popped = storage[sizeOf - 1];
+    if(sizeOf === 0) {
+      // nothing
+    } else {
+      delete storage[sizeOf - 1];
+    }
+    return popped;
   };
 
   someInstance.size = function() {
+    var count = 0;
+    for (var index in storage) {
+      count++;
+    }
+    return count;
   };
 
   return someInstance;
