1bb1eed503ea9075fb2a5406de0240a520653a36
MKS-students <mks.students@hackreactor.com>
2016-10-28 12:15:44 -0500

    Finished trees.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  newTree.addChild = treeMethods.addChild;
+  newTree.contains = treeMethods.contains;
 
   return newTree;
 };
@@ -11,9 +11,29 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newChild = Tree(value);
+  this.children[this.children.length] = newChild;
+
 };
 
 treeMethods.contains = function(target) {
+  var value = this.value;
+  var result;
+  if ( value === target) {
+    return true;
+  }
+  else {
+    if (this.children.length === 0) {
+      return;
+    }
+    for ( var i = 0; i < this.children.length; i ++) {
+      result = this.children[i].contains(target);
+      if(result === true) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
 
 
